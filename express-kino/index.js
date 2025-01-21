2e9619cc0965e798d838fa55582babdbee9ce1b0
Jan Elia <jan.elia995@hotmail.com>
2025-01-21 15:53:26 +0100

    adding the main page and the movie page to the content for SSR

--- express-kino/index.js
+++ express-kino/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/', async (request, response) => {
+    const buf = await fs.readFile('./content/index.html');
+    const text = buf.toString();
+
+    response.send(text);
 });
 
 app.listen(5080);
