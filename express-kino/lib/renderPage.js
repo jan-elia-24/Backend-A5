b4c78d9e73e2d689fe1045f07782ddc65815d23b
Jan Elia <jan.elia995@hotmail.com>
2025-01-24 00:12:08 +0100

    change the ordinary html to handlebars

--- express-kino/lib/renderPage.js
+++ express-kino/lib/renderPage.js
@@ -2,15 +2,7 @@ import fs from 'fs/promises';
 
 export default async function renderPage(response, page) {
     try {
-        const contentBuf = await fs.readFile(`./content/${page}.html`);
-        const contentText = contentBuf.toString();
-
-        const templateBuf = await fs.readFile('./templates/main.html');
-        const templateText = templateBuf.toString();
-
-        const outputHtml = templateText.replace('Hello', contentText);
-
-        response.send(outputHtml);
+        response.render(page);
     } catch (error) {
         console.error(`Error rendering page "${page}":`, error);
         response.status(500).send('An error occurred while rendering the page.');
