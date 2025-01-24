b565bdb59d1f7f8701be9ea71eb77f14bc41f16c
Jan Elia <jan.elia995@hotmail.com>
2025-01-24 20:49:45 +0100

    loading movies from api now

--- express-kino/script/getMovies.js
+++ express-kino/script/getMovies.js
@@ -1,59 +1,15 @@
-const movies = {
-    allMovies: [], 
-    released: [], 
-    upcoming: [],
-    //Function that adds all movies to an array
-    async getAllMovies() {
-        
-        fetch("content/movies.json")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            data.forEach(element => {
-                this.allMovies.push(element);
-            });
-        })
-        .catch (error => {
-            console.error('Problem fetching:', error);
-        });
-    },
+import fetch from "node-fetch";
 
-    //Function that adds released movies to an array
-    async getReleasedMovies() {
+const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 
-        fetch("content/movies.json")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            data.forEach(element => {
-                if (!element.comingSoon) //Checking if comingSoon is false
-                this.released.push(element);
-            });
-        })
-        .catch (error => {
-            console.error('Problem fetching:', error);
-        });
-    },
+export async function loadMovies() {
+  const res = await fetch(API_BASE + '/movies');
+  const payload = await res.json();
+  return payload.data;
+}
 
-    //Function that adds upcoming movies to an array
-    async getUpcomingMovies() {
-
-        fetch("content/movies.json")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            data.forEach(element => {
-                if (element.comingSoon) //Checking if comingSoon is true
-                this.upcoming.push(element);
-            });
-        })
-        .catch (error => {
-            console.error('Problem fetching:', error);
-        });
-    }
-};
-
-export { movies };
+export async function loadMovie(id) {
+  const res = await fetch(API_BASE + '/movies/' + id);
+  const payload = await res.json();
+  return payload.data;
+}
